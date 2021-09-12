8ea87906eac7f774bfcd07253d49c951720466e3
manuel864 <manuel-alb@web.de>
2021-09-12 16:08:07 +0200

    spear

--- 3Objekte/Speer/Speer.js
+++ 3Objekte/Speer/Speer.js
@@ -59,42 +59,42 @@ async function createShaderProgram(gl, vertexText, fragmentText){
 function createSpeerHeadBuffer(gl){
 
     var headVertices = 
-	[ // X, Y, Z           R, G, B
+	[ // X, Y, Z           	S, T 		Normalen			R, G, B, A
 		// Top
-		-3.4, 0.4, -0.0,   1.0, 1.0, 1.0,
-		-3.4, 0.4, 0.0,    1.0, 1.0, 1.0,
-		3.4, 0.0, 0.0,     1.0, 1.0, 1.0,
-		3.4, 0.0, 0.0,    1.0, 1.0, 1.0,
+		-3.4, 0.4, -0.0,   	0, 0,		0.0,0.0,0.0,		1.0, 1.0, 1.0, 1.0,
+		-3.4, 0.4, 0.0,    	0, 0,		0.0,0.0,0.0,		1.0, 1.0, 1.0, 1.0,
+		3.4, 0.0, 0.0,     	0, 0,		0.0,0.0,0.0,		1.0, 1.0, 1.0, 1.0,
+		3.4, 0.0, 0.0,    	0, 0,		0.0,0.0,0.0,		1.0, 1.0, 1.0, 1.0,
 
 		// Left
-		-3.4, 0.4, 0.4,    0.8, 0.8, 0.8,
-		-3.4, -0.4, 0.4,   0.8, 0.8, 0.8,
-		-3.4, -0.4, -0.4,  0.8, 0.8, 0.8,
-		-3.4, 0.4, -0.4,   0.8, 0.8, 0.8,
+		-3.4, 0.4, 0.4,    	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		-3.4, -0.4, 0.4,  	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0, 
+		-3.4, -0.4, -0.4,  	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		-3.4, 0.4, -0.4,   	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
 
 		// Right
-		3.4, 0.0, 0.0,     0.8, 0.8, 0.8,
-		3.4, 0.0, 0.0,    0.8, 0.8, 0.8,
-		3.4, 0.0, 0.0,   0.8, 0.8, 0.8,
-		3.4, 0.0, 0.0,    0.8, 0.8, 0.8,
+		3.4, 0.0, 0.0,     	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		3.4, 0.0, 0.0,     	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		3.4, 0.0, 0.0,     	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		3.4, 0.0, 0.0,     	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
 
 		// Front
-		3.4, 0.0, 0.0,     0.8, 0.8, 0.8,
-		3.4, 0.0, 0.0,    0.8, 0.8, 0.8,
-		-3.4, -0.4, 0.4,   0.8, 0.8, 0.8,
-		-3.4, 0.4, 0.4,    0.8, 0.8, 0.8,
+		3.4, 0.0, 0.0,     	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		3.4, 0.0, 0.0,     	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		-3.4, -0.4, 0.4,   	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		-3.4, 0.4, 0.4,    	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
 
 		// Back
-		3.4, 0.0, -0.0,    0.8, 0.8, 0.8,
-		3.4, 0.0, -0.0,   0.8, 0.8, 0.8,
-		-3.4, -0.4, -0.4,  0.8, 0.8, 0.8,
-		-3.4, 0.4, -0.4,   0.8, 0.8, 0.8,
+		3.4, 0.0, -0.0,    	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		3.4, 0.0, -0.0,    	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		-3.4, -0.4, -0.4,  	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		-3.4, 0.4, -0.4,   	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
 
 		// Bottom
-		-3.4, -0.4, -0.4,   0.8, 0.8, 0.8,
-		-3.4, -0.4, 0.4,    0.8, 0.8, 0.8,
-		3.4, 0.0, 0.0,     0.8, 0.8, 0.8,
-		3.4, 0.0, 0.0,    0.8, 0.8, 0.8,
+		-3.4, -0.4, -0.4,  	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		-3.4, -0.4, 0.4,   	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		3.4, 0.0, 0.0,     	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
+		3.4, 0.0, 0.0,     	0, 0,		0.0,0.0,0.0,		0.8, 0.8, 0.8, 1.0,
 	];
 
 	var headIndices =
@@ -125,11 +125,11 @@ function createSpeerHeadBuffer(gl){
 	];
 
     var headVertexBufferObject = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject );
+    gl.bindBuffer(gl.ARRAY_BUFFER, headVertexBufferObject );
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(headVertices), gl.STATIC_DRAW);
 
     var headIndexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, headIndexBufferObject);
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(headIndices), gl.STATIC_DRAW);
 
     return{
@@ -154,7 +154,7 @@ async function createGripDrawable(gl){
         gl.useProgram(program);
 
         let positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-		let colorAtrributeLocation = gl.getAttribLocation(program, 'vertColor');
+		let normalAtrributeLocation = gl.getAttribLocation(program, 'vertNormal');
 		gl.bindBuffer(gl.ARRAY_BUFFER, drawable.vbo);
 		gl.vertexAttribPointer(
 			positionAttribLocation, // Attribute location
@@ -165,8 +165,8 @@ async function createGripDrawable(gl){
 			0 // Offset from the beginning of a single vertex to this attribute
 		);
 		gl.vertexAttribPointer(
-			colorAtrributeLocation, // Attribute location
-			2, // Number of elements per attribute
+			normalAtrributeLocation, // Attribute location
+			3, // Number of elements per attribute
 			gl.FLOAT, // Type of elements
 			gl.FALSE,
 			8 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
@@ -174,7 +174,7 @@ async function createGripDrawable(gl){
 		);
 		
         gl.enableVertexAttribArray(positionAttribLocation);
-        gl.enableVertexAttribArray(colorAtrributeLocation);
+        gl.enableVertexAttribArray(normalAtrributeLocation);
 
         gl.drawArrays(gl.TRIANGLES, 0, vertices.length/8);
     }
@@ -186,7 +186,7 @@ async function createGripDrawable(gl){
 function drawObject(gl, program, buffers){
 
     var positionAtrributeLocation = gl.getAttribLocation(program, "vertPosition");
-    var colorAtrributeLocation = gl.getAttribLocation(program, "vertColor");
+    var normalAtrributeLocation = gl.getAttribLocation(program, "vertNormal");
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.vbo);
     gl.vertexAttribPointer(
         positionAtrributeLocation, // location
@@ -198,16 +198,16 @@ function drawObject(gl, program, buffers){
     );
 
     gl.vertexAttribPointer(
-        colorAtrributeLocation, // location
+        normalAtrributeLocation, // location
         3, //number of elem per Attributes
         gl.FLOAT, // Type of Elem
         gl.FALSE,
         6 * Float32Array.BYTES_PER_ELEMENT, // size of individual Vertex
-        3 * Float32Array.BYTES_PER_ELEMENT // Offset from beginning of a single vertex to this Attribute
+        5 * Float32Array.BYTES_PER_ELEMENT // Offset from beginning of a single vertex to this Attribute
     );
 
     gl.enableVertexAttribArray(positionAtrributeLocation);
-    gl.enableVertexAttribArray(colorAtrributeLocation);
+    gl.enableVertexAttribArray(normalAtrributeLocation);
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.ibo);
 	gl.drawElements(gl.TRIANGLES, buffers.length, gl.UNSIGNED_SHORT, 0);
@@ -218,10 +218,10 @@ function drawObject(gl, program, buffers){
 async function init() {
     var canvas = document.getElementById("gl-canvas");
     var gl = getGlContext(canvas);
-    var program = await createShaderProgram(gl, "speer_vert.glsl", "speer_frag.glsl");
+    var program = await createShaderProgram(gl, "spear_vert.glsl", "spear_frag.glsl");
     gl.useProgram(program);
 
-    var paintingBuffer = createBoxBuffer(gl);
+    var headBuffer = createSpeerHeadBuffer(gl);
 
     var grip = await createGripDrawable(gl);
 
@@ -229,17 +229,65 @@ async function init() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); 
 	gl.enable(gl.DEPTH_TEST);
 
+	var material = {
+		 baseColor: [0.2, 0.4, 0.65, 1.0],
+		 shini: 30.0, 
+		 spec: 1.0, 
+		 ambient: [0.05,0.05,0.2]
+	};
+
+	var pointLight1 = {
+		Pos: [0.0, -7.5, 0.0], 
+		Color: [0.0, 0.0, 0.5], 
+		SpecColor: [1.0, 1.0, 1.0]
+	}
+
+	var pointLight2 = {
+		Pos: [0.0, 10.0, 0.0], 
+		Color: [0.6, 0.0, 0.], 
+		SpecColor: [1.0, 0.0, 0.0]
+	}
+
+
+	    //Material setUp
+		let baseColorLocation = gl.getUniformLocation(program, 'mat.color');
+		let specLocation = gl.getUniformLocation(program, 'mat.spec')
+		let shininessLocation = gl.getUniformLocation(program, 'mat.shininess');
+		let ambientLocation = gl.getUniformLocation(program, 'mat.ambient');
+		gl.uniform4fv(baseColorLocation, material.baseColor );
+		gl.uniform1f(specLocation, material.spec);
+		gl.uniform1f(shininessLocation, material.shini);
+		gl.uniform1f(ambientLocation, material.ambient);
+	
+	
+		//Erste Lampe
+		let lightColorLocation1 = gl.getUniformLocation(program, 'pointLight1.lightColor');
+		let lightPosLocation1 = gl.getUniformLocation(program, 'pointLight1.position')
+		let specColorLocation1 = gl.getUniformLocation(program, 'pointLight1.specColor');
+		gl.uniform3fv(lightPosLocation1,pointLight1.Pos);
+		gl.uniform3fv(lightColorLocation1, pointLight1.Color);
+		gl.uniform3fv(specColorLocation1, pointLight1.SpecColor);
+	
+		//Zweite Lampe
+		let lightColorLocation2 = gl.getUniformLocation(program, 'pointLight2.lightColor');
+		let lightPosLocation2 = gl.getUniformLocation(program, 'pointLight2.position')
+		let specColorLocation2 = gl.getUniformLocation(program, 'pointLight2.specColor');
+		gl.uniform3fv(lightPosLocation2,pointLight2.Pos );
+		gl.uniform3fv(lightColorLocation2, pointLight2.Color);
+		gl.uniform3fv(specColorLocation2, pointLight2.SpecColor);
+
     var matWorldUniformLocation = gl.getUniformLocation(program, "mWorld");
     var matViewUniformLocation = gl.getUniformLocation(program, "mView");
     var matProjUniformLocation = gl.getUniformLocation(program, "mProj");
+	var viewWorldPositionLocation = gl.getUniformLocation(program, "eyePosition");
 
     var worldMatrix = new Float32Array(16);
     var viewMatrix = new Float32Array(16);
     var projMatrix = new Float32Array(16);
     glMatrix.mat4.identity(worldMatrix);
-    glMatrix.mat4.lookAt(viewMatrix, [0, 0, 7], [0, 0, 0], [0, 1, 0]);
+    glMatrix.mat4.lookAt(viewMatrix, [0, 0, 8], [0, 0, 0], [0, 1, 0]);
     glMatrix.mat4.perspective(projMatrix, glMatrix.glMatrix.toRadian(45), canvas.width/canvas.height, 0.1, 1000.0);
-
+	gl.uniform3fv(viewWorldPositionLocation, [0, 0, 8]);
     gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
     gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
 
@@ -255,7 +303,7 @@ async function init() {
         glMatrix.mat4.translate(worldMatrix, worldMatrix, [3.25, 0.0, 0.0]);
         glMatrix.mat4.scale(worldMatrix, worldMatrix, [0.15, 0.15, 0.15]);
         gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-        drawObject(gl, program, paintingBuffer);
+        drawObject(gl, program, headBuffer);
 
 
 
---
+++ 3Objekte/Speer/spear_frag.glsl
@@ -0,0 +1,70 @@
+precision mediump float;
+
+struct Light {
+    vec3 position;
+    vec3 lightColor;
+    vec3 specColor;
+    vec3 direction;
+};
+
+
+struct Material {
+    vec4 color;
+    float shininess;
+    float spec;
+    vec3 ambient;
+};
+uniform Light pointLight1;
+uniform Light pointLight2;
+uniform Material mat;
+varying vec3 fragNormal;
+varying vec3 fPosition;
+
+//varying vec3 v_surfaceToPointLight [numberOfLights];
+
+
+uniform vec3 eyePosition;
+
+//varying vec3 v_surfaceToView;
+//varying vec3 fragColor;
+
+
+
+void main(){
+    gl_FragColor = mat.color;
+    vec3 normal = normalize(fragNormal); 
+    vec3 v_surfaceToView = normalize(eyePosition - fPosition);
+    vec3 finalLightColor = mat.ambient;
+    vec3 finalSpecColor = vec3(0.0,0.0,0.0);
+    /////////Erste Lampe
+    float specular = 0.0;
+    float light = 0.0;
+    vec3 v_surfaceToPointLight = normalize(pointLight1.position - fPosition); //L Vektor: Direction  
+    vec3 halfVector = normalize(v_surfaceToPointLight + v_surfaceToView);
+    light=dot(v_surfaceToPointLight, normal); 
+    if (light > 0.0) {
+        specular = pow(dot(normal, halfVector), 300.0);
+    }
+    float lightStrenght =  pow(max(dot(reflect(-v_surfaceToPointLight, normal), vec3(0.0,0.0,1.0)), 0.0), mat.shininess);
+    finalLightColor += mat.color.rgb*(light * pointLight1.lightColor);
+    finalSpecColor +=  mat.spec*lightStrenght * pointLight1.specColor;
+
+    ////////////////////Zweite Lampe
+    specular = 0.0;
+    light = 0.0;
+    v_surfaceToPointLight = normalize(pointLight2.position - fPosition); //L Vektor: Direction  
+    halfVector = normalize(v_surfaceToPointLight + v_surfaceToView);
+    light=dot(v_surfaceToPointLight, normal); 
+    if (light > 0.0) {
+        specular = pow(dot(normal, halfVector), 300.0);
+    }
+    lightStrenght =  pow(max(dot(reflect(-v_surfaceToPointLight, normal), vec3(0.0,0.0,1.0)), 0.0), mat.shininess);
+    finalLightColor += mat.color.rgb*(light * pointLight2.lightColor);
+    finalSpecColor +=  mat.spec*lightStrenght * pointLight2.specColor;
+
+
+
+
+    gl_FragColor = vec4(finalLightColor,mat.color.a);
+    gl_FragColor.rgb += finalSpecColor;
+}
